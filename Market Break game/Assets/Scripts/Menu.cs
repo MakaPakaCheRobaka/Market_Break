fb37436cf8f70fa5340ed89d64f542f6a9c94ced
Jiyam <makapakachcerobaka@o2.pl>
2017-04-07 13:42:02 +0200

    Nowe menu

--- Market Break game/Assets/Scripts/Menu.cs
+++ Market Break game/Assets/Scripts/Menu.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
 
-    public enum Choice {Start, Opcje, Wyjście };
+    public enum Choice {Start, Dźwięk, Muzyka, Wyjście };
     public Choice choice;
+	public string scena;
+
+	Ustawienia ust;
+
+	Text text;
 
 	// Use this for initialization
 	void Start () {
-       
+		ust = GameObject.Find ("Ustawienia").GetComponent<Ustawienia> ();
+		text = gameObject.GetComponentInChildren<Text>();
 	}
 	
 	// Update is called once per frame
@@ -20,15 +27,37 @@ public class Menu : MonoBehaviour {
 
     void OnMouseDown()
     {
-        if(choice == Choice.Start)
-        {
-            Debug.Log("1");
-            SceneManager.LoadScene("QTE_1_scene");
-        }
-        else if(choice == Choice.Opcje)
-        {
-            Debug.Log("2");
-        }
+		if (choice == Choice.Start) 
+		{
+			Debug.Log ("1");
+			SceneManager.LoadScene (scena);
+		} 
+		else if (choice == Choice.Dźwięk) 
+		{
+			if (ust.dzwiek == true) 
+			{
+				ust.dzwiek = false;
+				text.text = "DŹWIĘK: OFF";
+			}
+			else 
+			{
+				ust.dzwiek = true;
+				text.text = "DŹWIĘK: ON";
+			}
+		} 
+		else if (choice == Choice.Muzyka) 
+		{
+			if (ust.muzyka == true) 
+			{
+				ust.muzyka = false;
+				text.text = "MUZYKA: OFF";
+			}
+			else 
+			{
+				ust.muzyka = true;
+				text.text = "MUZYKA: ON";
+			}
+		}
         else
         {
             Debug.Log("3");
---
+++ Market Break game/Assets/Scripts/Ustawienia.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ustawienia : MonoBehaviour {
+
+	public bool dzwiek;
+	public bool muzyka;
+
+	void Awake ()
+	{
+		DontDestroyOnLoad (gameObject);
+		dzwiek = true;
+		muzyka = true;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
