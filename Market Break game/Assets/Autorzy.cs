fbb3548dc0d7caf72507ffe89ea5e3400ff20b23
Jejkop <kubaxson@gmail.com>
2017-05-08 02:19:39 +0200

    Market Break game

---
+++ Market Break game/Assets/Autorzy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Autorzy : MonoBehaviour {
+
+    public Text a1;
+    public Text a2;
+    public Text a3;
+    public Text a4;
+    // Use this for initialization
+    
+
+   public void Pokaż_autorów()
+    {
+        a1.text = "Jakub Bartkowiak ";
+        a2.text = "Jakub Cibail";
+        a3.text = "Aleksander Lewandowski";
+        a4.text = "Hubert Owocki";
+
+    }
+}
