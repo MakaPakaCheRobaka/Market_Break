cee0c946b8947b64f14a8b909e8b28df5b359fe6
Jiyam <makapakachcerobaka@o2.pl>
2017-05-10 21:44:20 +0200

    Przepisanie Destroy na C# + porządek w assetach

---
+++ Market Break game/Assets/Scripts/Destroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	public int destroyTime = 5;
+	Ustawienia settings;
+
+	// Use this for initialization
+	void Start () 
+	{
+		settings = GameObject.FindWithTag ("Ustawienia").GetComponent<Ustawienia> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!IsInvoking("DestroyObject"))
+		{
+			Invoke("DestroyObject", destroyTime);
+		}
+		if(settings.spawnerPause)
+		{
+			CancelInvoke("DestroyObject");
+		}
+	}
+
+	void DestroyObject()
+	{
+		Destroy(gameObject);
+	}
+}
